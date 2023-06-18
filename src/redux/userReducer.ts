f49ac7f75d27aa2baadd7ff6dfb7a14fa6c7365b
Athini Bashe <bashe.athini@gmail.com>
2023-06-18 16:44:29 +0200

    Update userReducer.ts

--- src/redux/userReducer.ts
+++ src/redux/userReducer.ts
@@ -1,4 +1,4 @@
-import {FOLLOW_USER, BLOCK_USER, UNFOLLOW_USER, CURRENT_VIEWING_USER} from './actionTypes';
+import {FOLLOW_USER, BLOCK_USER, UNFOLLOW_USER} from './actionTypes';
 
 const initialState = {
   following: [],
@@ -26,11 +26,6 @@ const userReducer = (
         ...state,
         following: state.following.filter(userId => userId !== action.payload),
       };
-    case CURRENT_VIEWING_USER: 
-      return {
-        ...state,
-        currentViewUser: action.payload
-      }
     default:
       return state;
   }
